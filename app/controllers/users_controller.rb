1925d0ed4d231dfdd1cdd03df5684e55d16d3cf1
Caio <caio.nevesleite@gmail.com>
2017-06-04 23:40:00 -0300

    Signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,28 @@ class UsersController < ApplicationController
   	end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+     @user = User.find(params[:id])
+  end
+
+  def update 
+     @user = User.find(params[:id])
+     if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
   private 
   def user_params
   	params.require(:user).permit(:name, :email)
