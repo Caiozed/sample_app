bed06f7931a64fb7d8ed5fc507c224db7df86c01
Caio <caio.nevesleite@gmail.com>
2017-06-05 23:11:35 -0300

    Use ssl and puma in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to root_url
+      flash[:success] = "Welcome to the Sample App!"
+  		redirect_to user_path(@user)
   	else
   		render :new
   	end
@@ -36,6 +37,6 @@ class UsersController < ApplicationController
 
   private 
   def user_params
-  	params.require(:user).permit(:name, :email)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ SampleApp::Application.routes.draw do
   get '/help',    to: 'static_pages#help'  
   get '/about',   to: 'static_pages#about' 
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
 end
