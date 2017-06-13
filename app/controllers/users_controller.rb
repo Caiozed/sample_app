65aec97a20b9a3bf52f097b6158d3892135d3c16
Caio <caio.nevesleite@gmail.com>
2017-06-13 20:59:32 -0300

    Account activation complete Fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
-    redirect_to root_url and return unless @user.activated?
   	if @user.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
@@ -25,6 +24,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def edit
